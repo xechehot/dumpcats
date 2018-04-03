4a4b4104d5a79769acf454bbe15d676e183c0621
Max Pomazuev <max.pomazuev@gmail.com>
2018-04-04 01:04:24 +0500

    WORKS!

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import sys
 import os
 import time
@@ -14,19 +15,25 @@ from fastai.dataset import *
 from fastai.sgdr import *
 from fastai.plots import *
 
+import cv2
+
+import io
 import numpy as np
 
 
 def open_image(file_object):
-    flags = cv2.IMREAD_UNCHANGED + cv2.IMREAD_ANYDEPTH + cv2.IMREAD_ANYCOLO
+    # flags = cv2.IMREAD_UNCHANGED + cv2.IMREAD_ANYDEPTH + cv2.IMREAD_ANYCOLO
+    flags = 5
+    file_object.seek(0)
     img_array = np.asarray(bytearray(file_object.read()), dtype=np.uint8)
-    im = cv2.imdecode(img_array, cv2_img_flag).astype(np.float32)/255
+    im = cv2.imdecode(img_array, flags).astype(np.float32) / 255
     return cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
+
 def transformed_image(file_object):
-    image = open_image(file_object)
+    # image = open_image(file_object)
     _, val_tfms = tfms_from_model(resnet34, 224)
-    source_img = open_image(image)
+    source_img = open_image(file_object)
     return val_tfms(source_img)
 
 
@@ -36,36 +43,28 @@ def predict(file_object):
     tm = torch.load('/app/dogs-cats-torch-model.pt',
                     map_location=lambda storage, loc: storage)
     tm.train(False)
-
-    x = Variable(torch.from_numpy(image), requires_grad=False)
+    print(image.shape)
+    x = Variable(torch.from_numpy(np.array([image])), requires_grad=False)
     output = tm(x.cpu())
-    return np.exp(output.cpu().data.numpy())
+    return np.exp(output.cpu().data.numpy())[0]
 
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     m = telepot.namedtuple.Message(**msg)
 
-    # print(m.photo)
-
-    # import ipdb; ipdb.set_trace();
-
-    # if chat_id < 0:
-    #     # group message
-    #     print('Received a %s from %s, by %s' % (content_type, m.chat, m.from_))
-    # else:
-    #     # private message
-    #     print('Received a %s from %s' % (content_type, m.chat))  # m.chat == m.from_
-
     if content_type == 'photo':
-        f = TemporaryFile('wb')
+        f = io.BytesIO()
         if len(m.photo) < 2:
-            reply = "Photo too small!"
+            reply = "Картинка слишком мала"
         else:
             bot.download_file(m.photo[1].file_id, f)
-            reply = predict(f)
+            cat, dog = predict(f)
+            reply = "Котик 🐱: {:.2%}\nСобачка 🐶: {:.2%}".format(cat, dog)
 
         bot.sendMessage(chat_id, reply)
+    else:
+        bot.sendMessage(chat_id, "Пришли мне картинку! 🏞")
 
 
 TOKEN = os.environ["BOT_TOKEN"]
